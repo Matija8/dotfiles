36135aac00fee33eb3ffc02fff82c6aee4659cfa
matija8 <matijanme@gmail.com>
2022-05-03 16:03:35 +0200

    fix: __clip on windows

--- scripts/lib/clipboard.sh
+++ scripts/lib/clipboard.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 # coding: UTF-8
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    __clip="xclip -selection clipboard"
+elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
+    __clip="clip"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    __clip="pbcopy"
+fi
+
 function printWorkingDirToClipboard {
     # https://stackoverflow.com/questions/3482289/easiest-way-to-strip-newline-character-from-input-string-in-pasteboard
-    pwd | tr -d '\n' | xclip -selection clipboard
+    pwd | tr -d '\n' | $__clip
 }
 
 function printQuotedWorkingDirToClipboard {
-    printf "\"$(pwd)\"" | xclip -selection clipboard
+    printf "\"$(pwd)\"" | $__clip
 }
 
 function printPathToClipboard {
-    printf "$(realpath $1)" | xclip -selection clipboard
+    printf "$(realpath $1)" | $__clip
 }
 
 function printQuotedPathToClipboard {
-    printf "\"$(realpath $1)\"" | xclip -selection clipboard
+    printf "\"$(realpath $1)\"" | $__clip
 }
