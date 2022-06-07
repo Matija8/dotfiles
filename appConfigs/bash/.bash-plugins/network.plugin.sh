1982b40961fed4cd20a872e1b4fbbd9032ad816e
matija8 <matijanme@gmail.com>
2022-06-07 20:31:40 +0200

    feat: add network plugin (ssh, ping)

---
+++ appConfigs/bash/.bash-plugins/network.plugin.sh
@@ -0,0 +1,5 @@
+# https://security.stackexchange.com/questions/234245/what-information-does-ssh-vvv-expose
+# https://stackoverflow.com/questions/50243139/how-ping-command-work
+
+alias sshv="ssh -vvv"   # Verbosity level 3 (highest) - For debugging
+alias ping5="ping -c 5" # Stop after 5 packets
