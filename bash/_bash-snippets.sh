246f17dc03ce492648971a27c52ecbee946d4313
matija8 <matijanme@gmail.com>
2022-08-08 12:31:34 +0200

    feat: Update bash notes

--- bash/_bash-snippets.sh
+++ bash/_bash-snippets.sh
@@ -4,11 +4,21 @@
 # Cd to script directory
 cd $(dirname "$0")
 
+# Cd to previous dir
+cd -
+
+# Print current working dir.
+# Good for checking if mkdir & cd worked
+pwd
+
 # Pipe stderr & stdout to file
 echo "Stuff" &>"./test-file.txt"
 
 # Discard stderr & stdout
 echo "Stuff" &>/dev/null
+
+# Sleep/wait
+sleep 5s
 
 # Clear file contents
 # https://superuser.com/questions/90008/how-to-clear-the-contents-of-a-file-from-the-command-line
--- bash/clear_bash_plugins.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# coding: UTF-8
-
-plugins_folder="$HOME/.bash-plugins"
-rm -rf plugins_folder
