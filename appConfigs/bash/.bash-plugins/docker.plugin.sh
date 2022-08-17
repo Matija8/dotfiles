36f8867bc2cd3774f7ed75ae273dfd6b48534f9d
matija8 <matijanme@gmail.com>
2022-08-17 12:23:54 +0200

    feat: docker plugin ps aliases

--- appConfigs/bash/.bash-plugins/docker.plugin.sh
+++ appConfigs/bash/.bash-plugins/docker.plugin.sh
@@ -4,8 +4,14 @@
 #     :
 # fi
 
+# To sanity check docker, run:
+# docker run hello-world
+
+alias docker-psa="docker ps -a"
+alias dpsa="docker ps -a"
+
 # https://stackoverflow.com/questions/20111063/bash-alias-command-with-both-single-and-double-quotes
-# Be careful with aliases that contain $() in them! Don't use "" quotes then!?
+# Be careful with aliases that contain $() in them! Don't use "" quotes then!
 alias docker-rm-all='docker rm -f $(docker ps -a -q)'
 # TODO: Don't require sudo on linux?
 alias docker-rm-allsu='sudo docker rm -f $(sudo docker ps -a -q)'
@@ -13,8 +19,13 @@ alias docker-rm-allsu='sudo docker rm -f $(sudo docker ps -a -q)'
 # If you get any errors, checkout:
 # https://docs.docker.com/engine/install/linux-postinstall
 
+# WSL2 Start Docker
+# https://stackoverflow.com/questions/44678725/cannot-connect-to-the-docker-daemon-at-unix-var-run-docker-sock-is-the-docker
+# https://unix.stackexchange.com/questions/269805/how-can-i-detach-a-process-from-a-bash-script
+
+alias docker-wsl2-start="sudo dockerd & disown"
+
 # TODO:
-# Docker ps
 # Docker attach
 # Docker exec ... bash
 # https://stackoverflow.com/questions/30172605/how-do-i-get-into-a-docker-containers-shell
