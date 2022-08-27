2f3209f506a18cc7fdeadb6531d83a8d82fa22f7
matija8 <matijanme@gmail.com>
2022-08-27 22:25:38 +0200

    feat: ubuntu ctrl+alt+lr shortcuts

---
+++ osLinux/ubuntu ctrl-alt-LR shortcuts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# coding: UTF-8
+
+# https://stackoverflow.com/questions/47808160/intellij-idea-ctrlaltleft-shortcut-doesnt-work-in-ubuntu
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "[]"
+
+# gsettings reset org.gnome.desktop.wm.keybindings switch-to-workspace-left
+# gsettings reset org.gnome.desktop.wm.keybindings switch-to-workspace-right
