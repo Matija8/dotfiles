4c91fd09d7a44360e80f98d8ed3819d5cc122852
matija8 <matijanme@gmail.com>
2022-08-30 01:16:39 +0200

    feat: git origin snippet

---
+++ scripts/snipetts-git/add-gitlab-origin.sh
@@ -0,0 +1,5 @@
+# Don't run this, this is just an example
+exit
+git remote add gitlab-origin git@gitlab.com:Matija8/Repo-slug-name.git
+git push -u gitlab-origin --all
+git push -u gitlab-origin --tags
