0aba3a0a62a2c1d0e575a1f4633bf9b1833fc748
matija8 <matijanme@gmail.com>
2022-08-29 11:43:39 +0200

    feat: duhs command

--- appConfigs/bash/.bash-plugins/shell.plugin.sh
+++ appConfigs/bash/.bash-plugins/shell.plugin.sh
@@ -12,3 +12,8 @@ alias chx="chmod +x"
 alias rmrf="rm -rf"
 # https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
 alias commandv="command -v"
+# https://unix.stackexchange.com/questions/185764/how-do-i-get-the-size-of-a-directory-on-the-command-line
+# https://serverfault.com/questions/715664/the-file-and-dir-sizes-of-du-hs-is-not-consistent-with-du-hs
+# dush: disk usage summarize human-readable
+alias dush="du -sh"
+alias duhs="du -sh"
