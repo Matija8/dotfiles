4e299b9a4300c3984a2d5272ba37b56401b26eb7
matija8 <matijanme@gmail.com>
2022-08-29 13:11:16 +0200

    feat: code golf

--- scripts/snippets-python/clash-of-code-helpers.py
+++ scripts/snippets-python/clash-of-code-helpers.py
@@ -12,3 +12,10 @@ def diff(a, b):
 
 def mid(a, b):
     return (a + b) // 2
+
+
+def use_input_as_print_for_code_golf():
+    I = input  # use uppercase I to differentiate from vars (a,b,c,r,s...)
+    # I=input
+    I(I())  # This 'prints' the first inputs return
+    # Super useful when you have more than 1 input line!
