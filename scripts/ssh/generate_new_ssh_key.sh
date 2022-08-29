f3927fb0e530c1ef757aa62f145e618ab6d500fc
matija8 <matijanme@gmail.com>
2022-08-30 01:47:12 +0200

    feat: ssh key generation script - show existing

--- scripts/ssh/generate_new_ssh_key.sh
+++ scripts/ssh/generate_new_ssh_key.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 # coding: UTF-8
 
+# GitHub docs:
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh
+
+# First check if you already have an ssh key
+printf "Existing public ssh key files:\n"
+ls -a ~/.ssh/*.pub
+printf "\n"
+
+read -p "Are you sure? (Ctrl+c to cancel)" yn
+
 # Generate public/private ed25519 key pair.
 ssh-keygen -t ed25519 -C "matijanme@gmail.com"
 
