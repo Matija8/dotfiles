b4b21671ba3a5f8190b25b9d9ab256da97c4be30
matija8 <matijanme@gmail.com>
2022-08-29 11:08:19 +0200

    feat: tst in /opt

---
+++ appConfigs/root-opt/_copy-to-opt.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# coding: UTF-8
+
+# The files sent to /opt from here should be used in shebang lines.
+# https://en.wikipedia.org/wiki/Shebang_(Unix)
+# https://stackoverflow.com/questions/10376206/what-is-the-preferred-bash-shebang
+
+sudo cp tst.sh /opt/tst
---
+++ appConfigs/root-opt/_rm-from-opt.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# coding: UTF-8
+
+sudo rm -f /opt/tst
