a88625bfaee165c85b7f0af8a27336316b75e801
matija8 <matijanme@gmail.com>
2022-09-05 03:11:56 +0200

    feat: git hooks stuff

---
+++ scripts/snippets-git/hooks/pre-commit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Hook should always start in the root of the repo?
+# pwd
+
+function exit_if_last_op_failed {
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+}
+
+npm run test
+# npm run bb # build/bundle BE
+# npm run bf # build/bundle FE
+exit_if_last_op_failed
