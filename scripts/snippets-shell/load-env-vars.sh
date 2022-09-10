9a8ce72c8610f8d4225ccabe5bd1c6d217a2b7ac
matija8 <matijanme@gmail.com>
2022-09-10 18:19:51 +0200

    feat: load env vars bash snippet

---
+++ scripts/snippets-shell/load-env-vars.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# coding: UTF-8
+
+# https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-value-pairs
+# https://gist.github.com/mihow/9c7f559807069a03e302605691f85572
+
+cd $(realpath $(dirname $0))
+pwd
+
+env_file_path="../env.txt"
+if [ -f "$env_file_path" ]; then
+    printf "Env file found.\n"
+    export $(cat "$env_file_path" | xargs)
+else
+    printf "Env file NOT found!\n"
+fi
+
+printf "$SOME_ENV_VAR\n\n"
+# printf "$GH_PAT\n\n"
+
+################################################################################
+# When you verify things works with the above version, then shorten it:
+
+env_file_path="$(realpath $(dirname $0))/../env.txt"
+if [ -f "$env_file_path" ]; then
+    export $(cat "$env_file_path" | xargs)
+else
+    printf "Env file NOT found!\n"
+fi
