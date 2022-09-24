5ad57fa0d6af90e787883177967fae3c83f123c4
matija8 <matijanme@gmail.com>
2022-09-24 23:58:47 +0200

    feat: jest (typescript + babel) notes

--- js/jest/ts-babel-jest👍.sh
+++
@@ -1,6 +0,0 @@
-# https://jestjs.io/docs/getting-started#using-babel
-# https://jestjs.io/docs/getting-started#using-typescript-via-babel
-
-yarn add -D babel-jest @babel/core @babel/preset-env @babel/preset-typescript
-# npm i -D babel-jest @babel/core @babel/preset-env @babel/preset-typescript
-# Prekopiraj ./babel.config.js u root projekta!
