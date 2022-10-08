5674cd0c9136e41bad3e6f92c99ecdb4a0d39e7b
matija8 <matijanme@gmail.com>
2022-10-08 14:46:51 +0200

    feat: string stuff

--- bash/string stuff.sh
+++ bash/string stuff.sh
@@ -6,10 +6,20 @@ echo $strA
 strB='bash'
 # strB='notbash'
 
+# https://linuxize.com/post/how-to-compare-strings-in-bash/
+# https://stackoverflow.com/questions/2237080/how-to-compare-strings-in-bash
+# https://unix.stackexchange.com/questions/67898/using-the-not-equal-operator-for-string-comparison
+
 if [ "$strA" = "$strB" ]; then
-    echo "Strings are equal."
+    echo "1a - Strings are equal."
 else
-    echo "Strings are not equal."
+    echo "1b - Strings are not equal."
 fi
+
+if [ "a" != "b" ]; then
+    echo "2 - Strings are not equal."
+fi
+
+if [ "a" != "b" ]; then echo "3 - Strings are not equal shorthand."; fi
 
 [[ "string1" == "string2" ]] && echo "Equal" || echo "Not equal"
