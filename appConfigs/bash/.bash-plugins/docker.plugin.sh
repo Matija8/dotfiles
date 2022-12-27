ec63b9d58509cb800e27d9dde96d1e580f805cdd
matija8 <matijanme@gmail.com>
2022-12-27 04:23:28 +0100

    feat: docker aliases

--- appConfigs/bash/.bash-plugins/docker.plugin.sh
+++ appConfigs/bash/.bash-plugins/docker.plugin.sh
@@ -7,11 +7,36 @@
 # To sanity check docker, run:
 # docker run hello-world
 # https://hub.docker.com/_/hello-world
-
-alias docker-psa="docker ps -a"
-alias dpsa="docker ps -a"
 alias docker-test="docker run hello-world"
+
+# You cannot start a container from a Dockerfile.
+# The process goes like this:
+# Dockerfile =[docker build]=> Docker image =[docker run]=> Docker container
+
+# Building images:
+# https://stackoverflow.com/questions/36075525/how-do-i-run-a-docker-instance-from-a-dockerfile
+# You can add -t TAG_NAME to add a tag to the image
+#
+# "docker build from pipe" mnemonic. Don't forget -t at the end!
+alias dbfp="docker build - <"
+# "docker build from current dir" mnemonic. Requires tag name at the end.
+alias dbfc="docker build . -t"
+
+# Running containers from images:
+alias drun="docker run" # Add TAG_NAME or ID at the end
+
+# Container ops:
+alias dpsa="docker ps -a" # List containers
+alias docker-psa="docker ps -a"
+alias drmf="docker rm -f" # Remove containers
+
+# Image ops:
+alias dim="docker image"
+alias dimls="docker image ls" # Same as `docker images`
 alias docker-images="docker images"
+# Deleting images
+alias dimrm="docker image rm"
+# https://docs.docker.com/engine/reference/commandline/image_rm/
 
 # https://stackoverflow.com/questions/20111063/bash-alias-command-with-both-single-and-double-quotes
 # Be careful with aliases that contain $() in them! Don't use "" quotes then!
