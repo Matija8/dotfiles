a0e7a32ab93037ab9556ab0381cb167cfb9f5aec
matija8 <matijanme@gmail.com>
2023-02-04 16:02:37 +0100

    feat: golang plugin

--- appConfigs/bash/.bash-plugins/golang.plugin.sh
+++ appConfigs/bash/.bash-plugins/golang.plugin.sh
@@ -7,4 +7,21 @@ if [ -d "/usr/local/go" ]; then
     # go version
 
     alias gorun="go run"
+
+    function gomodinit {
+        # Go packages docs:
+        # https://pkg.go.dev/about#best-practices
+        # https://go.dev/blog/using-go-modules
+        if [[ $# -eq 0 ]]; then
+            printf "\nPlease provide the Go module name.\n\n"
+            return
+        fi
+        if [ -d "$1" ]; then
+            printf "\nDir "$1" already exists.\n\n"
+            return
+        fi
+        mkdir $1
+        cd $1
+        go mod init $@
+    }
 fi
