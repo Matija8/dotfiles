b3950413a1ebc4cb5f91b0bef0e53b5a1ec7de5a
matija8 <matijanme@gmail.com>
2023-02-07 01:34:54 +0100

    feat: Docker!

--- appConfigs/bash/.bash-plugins/docker.plugin.sh
+++ appConfigs/bash/.bash-plugins/docker.plugin.sh
@@ -1,78 +1,84 @@
-# which docker &>/dev/null
-# if [ $? -eq "0" ]; then
-#     # Do stuff you would only do if docker was installed here
-#     :
-# fi
+if command -v docker &>/dev/null; then
 
-# To sanity check docker, run:
-# docker run hello-world
-# https://hub.docker.com/_/hello-world
-alias docker-test="docker run hello-world"
+    # To sanity check docker, run:
+    # docker run hello-world
+    # https://hub.docker.com/_/hello-world
+    alias docker-test="docker run hello-world"
 
-# You cannot start a container from a Dockerfile.
-# The process goes like this:
-# Dockerfile =[docker build]=> Docker image =[docker run]=> Docker container
+    # You cannot start a container from a Dockerfile.
+    # The process goes like this:
+    # Dockerfile =[docker build]=> Docker image =[docker run]=> Docker container
 
-# Building images:
-# https://stackoverflow.com/questions/36075525/how-do-i-run-a-docker-instance-from-a-dockerfile
-# You can add -t TAG_NAME to add a tag to the image
-#
-# "docker build from pipe" mnemonic. Don't forget -t at the end!
-alias dbfp="docker build - <"
-# "docker build from current dir" mnemonic. Requires tag name at the end.
-alias dbfc="docker build . -t"
+    # Building images:
+    # https://stackoverflow.com/questions/36075525/how-do-i-run-a-docker-instance-from-a-dockerfile
+    # You can add -t TAG_NAME to add a tag to the image
+    #
+    # "docker build from pipe" mnemonic. Don't forget -t at the end!
+    alias dbfp="docker build - <"
+    # "docker build from current dir" mnemonic. Requires tag name at the end.
+    alias dbfc="docker build . -t"
 
-# Running containers from images:
-#
-# For most of these commands, add a TAG_NAME or ID at the end
-alias drun="docker run"
-# Pawels command. Interactive, shell mode.
-# Prefer `docker exec` to `docker run` to not create too many containers.
-alias drunit="docker run -it --entrypoint ./bin/sh"
+    # Running containers from images:
+    #
+    # For most of these commands, add a TAG_NAME or ID at the end
+    alias drun="docker run"
 
-function dexecit {
-    # https://stackoverflow.com/questions/20813486/exploring-docker-containers-file-system
-    # https://docs.docker.com/engine/reference/commandline/exec/
-    # Must be run on an already running container.
-    local container_name="$1"
-    docker exec -it "$container_name" ./bin/sh
-}
+    function drunit {
+        # Pawels command. Interactive, shell mode.
+        # "docker run -it" starts a container and opens a shell (*runs a command).
+        # If you already have a running container, you can use "docker exec".
+        # Some containers exit right away (ubuntu), so "docker exec" doesn't work, only "docker run".
+        # Prefer `docker exec` to `docker run` if possible, to not create too many containers.
+        # By default opens "sh". When inside you can switch to bash, if your image has it.
+        #
+        # docker run --help
+        docker run -it --entrypoint ./bin/sh $@
+    }
 
-# Container ops:
-alias dpsa="docker ps -a" # List containers
-alias docker-psa="docker ps -a"
-alias drmf="docker rm -f" # Remove containers
+    function dexecit {
+        # https://stackoverflow.com/questions/30172605/how-do-i-get-into-a-docker-containers-shell
+        # https://stackoverflow.com/questions/20813486/exploring-docker-containers-file-system
+        # https://docs.docker.com/engine/reference/commandline/exec/
+        # Must be run on an already running container. Otherwise use "docker run -it".
+        #
+        # docker exec -it <mycontainer> bash
+        # docker exec --help
+        local container_name="$1"
+        docker exec -it "$container_name" ./bin/sh
+    }
 
-# Image ops:
-alias dim="docker image"
-alias dimage="docker image"
-# List images
-alias dims="docker image ls"
-alias dimls="docker image ls" # Same as `docker images`
-alias docker-images="docker images"
-# Deleting images
-alias dimrm="docker image rm"
-# https://docs.docker.com/engine/reference/commandline/image_rm/
+    # Container ops:
+    alias dpsa="docker ps -a" # List containers
+    alias docker-psa="docker ps -a"
+    alias drmf="docker rm -f" # Remove containers
 
-# https://stackoverflow.com/questions/20111063/bash-alias-command-with-both-single-and-double-quotes
-# Be careful with aliases that contain $() in them! Don't use "" quotes then!
-alias docker-stop-all='docker stop $(docker ps -aq)'
-alias docker-rm-all='docker rm -f $(docker ps -aq)'
-alias drmf-all='docker rm -f $(docker ps -aq)' # Remove all containers
-# TODO: Don't require sudo on linux?
-alias docker-rm-allsu='sudo docker rm -f $(sudo docker ps -aq)'
+    # Image ops:
+    alias dim="docker image"
+    alias dimage="docker image"
+    # List images
+    alias dims="docker image ls"
+    alias dimls="docker image ls" # Same as `docker images`
+    alias docker-images="docker images"
+    # Deleting images
+    alias dimrm="docker image rm"
+    # https://docs.docker.com/engine/reference/commandline/image_rm/
 
-# If you get any errors, checkout:
-# https://docs.docker.com/engine/install/linux-postinstall
+    # https://stackoverflow.com/questions/20111063/bash-alias-command-with-both-single-and-double-quotes
+    # Be careful with aliases that contain $() in them! Don't use "" quotes then!
+    alias docker-stop-all='docker stop $(docker ps -aq)'
+    alias docker-rm-all='docker rm -f $(docker ps -aq)'
+    alias drmf-all='docker rm -f $(docker ps -aq)' # Remove all containers
+    # TODO: Don't require sudo on linux?
+    alias docker-rm-allsu='sudo docker rm -f $(sudo docker ps -aq)'
 
-# WSL2 Start Docker
-# https://stackoverflow.com/questions/44678725/cannot-connect-to-the-docker-daemon-at-unix-var-run-docker-sock-is-the-docker
-# https://unix.stackexchange.com/questions/269805/how-can-i-detach-a-process-from-a-bash-script
-#
-# alias docker-wsl2-start="sudo dockerd & disown"
-alias docker-wsl2-start="sudo dockerd"
+    # If you get any errors, checkout:
+    # https://docs.docker.com/engine/install/linux-postinstall
 
-# TODO:
-# Docker attach
-# Docker exec ... bash
-# https://stackoverflow.com/questions/30172605/how-do-i-get-into-a-docker-containers-shell
+    # WSL2 Start Docker
+    # https://stackoverflow.com/questions/44678725/cannot-connect-to-the-docker-daemon-at-unix-var-run-docker-sock-is-the-docker
+    # https://unix.stackexchange.com/questions/269805/how-can-i-detach-a-process-from-a-bash-script
+    #
+    # alias docker-wsl2-start="sudo dockerd & disown"
+    alias docker-wsl2-start="sudo dockerd"
+
+fi
--- osLinux/test_setup_all.dockerfile
+++ osLinux/test_setup_all.dockerfile
@@ -11,12 +11,22 @@ FROM ubuntu
 # https://stackoverflow.com/questions/20635472/using-the-run-instruction-in-a-dockerfile-with-source-does-not-work
 SHELL ["/bin/bash", "-c"]
 
+# https://stackoverflow.com/questions/34571711/cant-run-curl-command-inside-my-docker-container
+RUN apt -y update; apt -y install curl zip vim
+
+# https://unix.stackexchange.com/questions/711574/ignore-sudo-for-each-command
+# RUN alias sudo=""
+RUN printf '#!/bin/bash\n$@' >/bin/sudo; chmod +x /bin/sudo
+
 RUN source <(curl -sL https://raw.githubusercontent.com/Matija8/dotfiles/main/osLinux/setup_all.sh)
 
-# To verify it was created
+# To verify the "dotfiles_setup" image was created:
 # docker images
 
-# To run image as container
+# To run image as container ❌:
 # docker run dotfiles_setup
+
+# To run image as container interactively 👍:
+# docker run -it --entrypoint ./bin/bash dotfiles_setup
 
 # TODO: Attach to container and debug
