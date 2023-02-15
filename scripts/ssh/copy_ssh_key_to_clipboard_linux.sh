af78308488b2c92896423a501c25adeb0b2c83cd
matija8 <matijanme@gmail.com>
2023-02-16 00:02:07 +0100

    feat: Remove copy to clipboard scripts

--- scripts/ssh/copy_ssh_key_to_clipboard_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# coding: UTF-8
-
-sudo apt install -y xclip
-# Copy the contents of the id_ed25519.pub file to clipboard.
-cat ~/.ssh/id_ed25519.pub
-xclip -selection clipboard <~/.ssh/id_ed25519.pub
