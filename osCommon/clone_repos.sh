b76e257b10a730480bab8fabe1d99ae993850bb3
matija8 <matijanme@gmail.com>
2023-02-16 02:36:57 -0800

    chore: formatting

--- osCommon/clone_repos.sh
+++ osCommon/clone_repos.sh
@@ -3,18 +3,24 @@
 
 projects_dir="$HOME/Projects"
 documents_dir="$HOME/Documents"
-git_ssh_base="git@github.com:Matija8"
+
+function git_clone_via_ssh {
+    git clone "git@github.com:Matija8/$1.git"
+    echo -e ""
+}
 
 # You need to have ssh keys setup on GitHub before running this script!
 
 mkdir -p "$projects_dir"
 if cd "$projects_dir"; then
-    git clone "$git_ssh_base/dotfiles.git"
-    git clone "$git_ssh_base/Random-Projects.git"
-    git clone "$git_ssh_base/FE-Widget-Experiments.git"
+    git_clone_via_ssh 'dotfiles'
+    git_clone_via_ssh 'Random-Projects'
+    git_clone_via_ssh 'FE-Widget-Experiments'
+    cd - &>/dev/null
 fi
 
 mkdir -p "$documents_dir"
 if cd "$documents_dir"; then
-    git clone "$git_ssh_base/Private-Docs.git"
+    git_clone_via_ssh 'Private-Docs'
+    cd - &>/dev/null
 fi
