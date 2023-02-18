37b01e514a212a9f2a969e65b20b0c063ae8c44c
matija8 <matijanme@gmail.com>
2023-02-18 18:40:56 +0100

    chore: Remove all js notes

--- js/jest-ts-babel/ts-jest-bad👎.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# coding: UTF-8
-
-# https://jestjs.io/docs/getting-started#using-typescript
-# https://www.npmjs.com/package/ts-jest
-
-yarn add -D jest typescript ts-jest @types/jest
-
-npx ts-jest config:init
