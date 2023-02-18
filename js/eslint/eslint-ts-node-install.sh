5f5a42324eb09f881111a2322f5ddb6dfe6e74ef
matija8 <matijanme@gmail.com>
2023-02-18 18:19:06 +0100

    feat: move eslint to random-projects

--- js/eslint/eslint-ts-node-install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# coding: UTF-8
-
-yarn add -D eslint typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin
-# npm i --save-dev eslint typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin
--- js/eslint/lint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# coding: UTF-8
-
-eslint . --ext .js,.jsx,.ts,.tsx
-
-# Package.json:
-# "lint": "eslint \"**/*.{js,ts,tsx}\""
