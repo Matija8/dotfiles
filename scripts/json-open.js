2b8722b70ebda0c7491303ce84a10e655992f9a9
matija8 <matijanme@gmail.com>
2023-06-03 03:38:48 +0200

    feat: remove unused scripts

--- scripts/json-open.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const fs = require('fs');
-
-// Use this script to quickly analyze json files.
-
-const fpath = './TODO.json';
-const data = JSON.parse(fs.readFileSync(fpath));
-console.log(data.length);
