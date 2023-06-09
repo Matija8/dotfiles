d81fe2c6bd0ab15f6e1ae68a66057e88ea798848
matija8 <matijanme@gmail.com>
2023-06-09 14:20:55 +0200

    feat: Mongo ping script

--- scripts/bin/mongoping.ts
+++ scripts/bin/mongoping.ts
@@ -3,24 +3,22 @@
 import { MongoClient } from 'https://deno.land/x/mongo/mod.ts';
 
 const client = new MongoClient();
+await client.connect('mongodb://localhost:27017');
+console.log('Connecting successful');
+const dbs = await client.listDatabases();
+console.log(
+  'databases:',
+  dbs.map(
+    (db) =>
+      `${db.name}    ${
+        db.sizeOnDisk && (db.sizeOnDisk / 1024 / 1024 / 1024).toFixed(4)
+      } GB`,
+  ),
+);
 
-try {
-  await client.connect('mongodb://localhost:27017');
-  console.log('Connecting successful');
-  const dbs = await client.listDatabases();
-  console.log({ dbs });
-
-  if (0) {
-    await Promise.all(
-      dbs.map(async (db) => {
-        const database = client.database(db.name);
-        const collections = await database.listCollectionNames();
-        console.log({ collections });
-      }),
-    );
-  }
-} catch (err) {
-  console.error('error', { err });
-} finally {
-  client.close();
+if (0) {
+  dbs.map(async (db) => {
+    const collections = await client.database(db.name).listCollectionNames();
+    console.log({ db: db.name, collections });
+  });
 }
