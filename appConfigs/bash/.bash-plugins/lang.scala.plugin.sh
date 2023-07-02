94978705d34a7d97fc8247ec3d73a3647e9c39d9
matija8 <matijanme@gmail.com>
2023-07-02 17:43:37 +0200

    feat: scala comment

--- appConfigs/bash/.bash-plugins/lang.scala.plugin.sh
+++ appConfigs/bash/.bash-plugins/lang.scala.plugin.sh
@@ -7,6 +7,8 @@ if cs version &>/dev/null; then
     # To check scala installation was successful:
     # cs version && scalac -version && scala -version && sbt -V
 
+    # Create an sbt project:
+    # https://docs.scala-lang.org/getting-started/#create-a-hello-world-project-with-sbt
     alias sbtnew="sbt new scala/hello-world.g8"
     alias sbtnew3="sbt new scala/scala3.g8"
 fi
