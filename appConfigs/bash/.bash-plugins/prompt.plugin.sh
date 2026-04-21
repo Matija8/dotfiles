c9f1f5460156b83986bff7d9482f233bc2ac93dc
matija8 <matijanme@gmail.com>
2026-04-21 16:20:05 +0200

    new bash prompt

--- appConfigs/bash/.bash-plugins/prompt.plugin.sh
+++ appConfigs/bash/.bash-plugins/prompt.plugin.sh
@@ -7,7 +7,12 @@ prompt_iife() {
     local PRMPT_CLR_RED="\[\e[31m\]"
     local PRMPT_CLR_GREEN="\[\e[32m\]"
     local PRMPT_CLR_BLUE="\[\e[34m\]"
-    local prompt_matija_1="$PRMPT_CLR_BLUE$prompt_time_hhmm $PRMPT_CLR_GREEN[$prompt_pwd]$PRMPT_CLR_RED\$ $PRMPT_CLR_NC"
+
+    local blue_time_str="$PRMPT_CLR_BLUE$prompt_time_hhmm"
+    local green_pwd_str="$PRMPT_CLR_GREEN[$prompt_pwd]"
+    local red_dollar_str="$PRMPT_CLR_RED ~>"
+    # The \n shows the prompt on a new line.
+    local prompt_matija_1="$blue_time_str $green_pwd_str\n$red_dollar_str $PRMPT_CLR_NC"
 
     # Prompt.
     export PS1="$prompt_matija_1"
